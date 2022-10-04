d3f66ca14842c44b57ec5ae1c43e35a05b49c192
rickie <rickiemaweu@gmail.com>
2022-10-05 00:38:25 +0300

    done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,4 @@
 class MoviesController < ApplicationController
-  wrap_parameters false
   
   def index
     movies = Movie.all
@@ -8,13 +7,14 @@ class MoviesController < ApplicationController
 
   def create
     movie = Movie.create(movie_params)
-    render json: movie, status: :created
+  render json: movie, status: :created
+
   end
 
   private
 
-  def movie_params
-    params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
-  end
-  
+def movie_params
+  params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+
 Rails.application.routes.draw do
-  resources :movies, only: [:index, :create]
+  resources :movies, only: [:index,:create]
 end
